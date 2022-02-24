435a7809628f0590d0ebe57a10d94fefbe0d16f3
sarmilaupadhyaya <sarmilaupadhyaya43@gmail.com>
2022-02-24 16:26:06 +0100

    dataset

---
+++ data/split_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+train_df =  pd.read_csv("train.csv")
+train_df, test_df = train_test_split(train_df, test_size=0.2)
+train_df.to_csv("train_vsl.csv")
+train_df, val_df = train_test_split(train_df, test_size=0.2)
+
+train_df.to_csv("train.csv")
+test_df.to_csv("test.csv")
+val_df.to_csv("val.csv")
+
